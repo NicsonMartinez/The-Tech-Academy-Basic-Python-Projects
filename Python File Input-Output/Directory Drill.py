12efbeea21c75cd9fb3aa8682a4c57ddf6ac9cd3
Nicson Martinez <martineznicson@gmail.com>
2019-05-10 17:37:58 -0700

    Update Directory Drill
    Took off what wasn't needed for the drill and cleaned out the code by making iterations through one for loop insted of  three. This code is also more efficient because it will print out the nth amount of txt files there are in a specific directory.

--- Python File Input-Output/Directory Drill.py
+++ Python File Input-Output/Directory Drill.py
@@ -4,38 +4,37 @@
 # verify whether those files end with a “.txt” file extension and if they do, print those qualifying
 # file names and their corresponding modified time-stamps to the console.
 #
+# Requirements:
+# Your script will need to use Python 3 and the OS module.
+#
+# Your script will need to use the listdir() method from the OS module to iterate through all files
+# within a specific directory.
+# 
+# Your script will need to use the path.join() method from the OS module to concatenate the file name to
+# its file path, forming an absolute path.
+# 
+# Your script will need to use the getmtime() method from the OS module to find the latest date that each
+# text file has been created or modified.
+# 
+# Your script will need to print each file ending with a “.txt” file extension and its corresponding mtime
+# to the console.
+#
 # By: Nicson Martinez
 # Date: 5/9/2019
 
 import os
 from datetime import datetime
 
-# Here I am listing 
+
 fPath = 'C:\\test-directory\\'
-directoryList = os.listdir(fPath) 
+directoryList = os.listdir(fPath) #Creates list of files in a specific directory.
 print("These are the files in the path, C:\\test-directory\\ :\n\n{}\n\n".format(directoryList))
 
-#This finds the 
-txtFilesList = []
-for i in directoryList:
-    if i.endswith('.txt'):
-        txtFilesList.append(i)
-print("Here is a list of only files that end in '.txt' :\n\n{}\n\n".format(txtFilesList))
+for file in directoryList:
+    if file.endswith('.txt'):
+        abPath = os.path.join(fPath, file) #Concatinates the path directory with a txt file found through each iteration. 
+        fModTime = os.path.getmtime(abPath) #Gets last modified time.
+        formattedTime = datetime.fromtimestamp(fModTime).strftime('%m-%d-%Y %H:%M:%S') #formats the time in a way that humans can understand
+        print("File {} : Last-Modified Time {}\n".format(abPath,formattedTime))
 
-pathsList = []
-for i in txtFilesList:
-    abPath = os.path.join(fPath, i)
-    pathsList.append(abPath)
-
-file1 = pathsList[0]
-time1 = os.path.getmtime(pathsList[0])
-file1Time = datetime.fromtimestamp(time1).strftime('%Y-%m-%d %H:%M:%S')
-
-file2 = pathsList[1]
-time2 = os.path.getmtime(pathsList[1])
-file2Time = datetime.fromtimestamp(time2).strftime('%Y-%m-%d %H:%M:%S')
-
-print("The following includes the absolute path of each file found and the \ndate & time that each file was last modified: \n")
-
-print("1. {}: {}\n2. {} : {}\n".format(file1,file1Time,file2,file2Time))
 
